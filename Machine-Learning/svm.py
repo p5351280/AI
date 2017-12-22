a4417c4b2b3ef5070889789ac51daa3629050d43
p5351280 <p5351280@gmail.com>
2017-12-23 00:59:38 +0800

    SVM using cross validation

--- Machine-Learning/svm.py
+++ Machine-Learning/svm.py
@@ -8,7 +8,7 @@ Created on Thu Dec 21 19:35:56 2017
 
 import numpy as np
 import pandas
-from sklearn import svm, cross_validation, preprocessing
+from sklearn import svm, model_selection, preprocessing, metrics
 
 DATA = pandas.read_csv('TraData.csv', dtype={'click': np.float64}).as_matrix()
 
@@ -22,29 +22,31 @@ for i in range(0, 12):
 
 DATA = np.array(DATA, dtype='float')
 
-test = DATA[861457:961457, 0:12]
-test_ans = DATA[861457:961457, 12:13]
+DATA_X = DATA[:, 0:12]
+DATA_Y = DATA[:, 12:13]
 
+train, test, train_ans, test_ans = model_selection.train_test_split(
+                                    DATA_X, DATA_Y, test_size = 0.2)
 
-for j in range(700000, 961457):
-    train = DATA[0:j, 0:12]
-    train_ans = DATA[0:j, 12:13]
-    clf = svm.SVC(kernel='rbf', C = 100, random_state=0, gamma = 'auto')
-    clf.fit(train, train_ans.ravel())
+clf = svm.SVC(kernel='rbf', C = 100, random_state=0, gamma = 'auto')
+clf.fit(train, train_ans.ravel())
 
-    pre_ans = clf.predict(test)
+pre_ans = clf.predict(test)
 
-    pre_cnt = 0
-    true_cnt = 0
-    get_cnt = 0
+pre_cnt = 0
+true_cnt = 0
+get_cnt = 0
     
-    for i in range(100000):
-        if(test_ans[i]==1 and pre_ans[i]==1):
-            get_cnt += 1
-        if(test_ans[i]==1):
-            true_cnt += 1
-        if(pre_ans[i] == 1):
-            pre_cnt += 1
+for i in range(0, len(test_ans)):
+    if(test_ans[i]==1 and pre_ans[i]==1):
+        get_cnt += 1
+    if(test_ans[i]==1):
+        true_cnt += 1
+    if(pre_ans[i] == 1):
+        pre_cnt += 1
 
-    print("{}: pre:{}%  recall:{}%".format(j, int(get_cnt/pre_cnt*100), 
-                                              int(get_cnt/true_cnt*100)))
+accuracy = metrics.accuracy_score(test_ans, pre_ans)
+print(accuracy)
+
+print("pre:{}%  recall:{}%".format(int(get_cnt/pre_cnt*100), 
+                                   int(get_cnt/true_cnt*100)))
